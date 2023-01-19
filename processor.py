136bbe4ad0dcfc2cb1e650d800d69ba1c2a7b053
Marek Ruszczyk <kirek007@gmail.com>
2023-01-19 12:20:43 +0100

    Implement multithreading for png generation

--- processor.py
+++ processor.py
@@ -1,23 +1,21 @@
-import argparse
 import cProfile
+from concurrent.futures import ThreadPoolExecutor
 import io
 import logging
+import multiprocessing
 import os
 from datetime import datetime
-from pathlib import Path
 from pstats import SortKey
 import pstats
+import queue
 from struct import unpack
-from subprocess import TimeoutExpired
 from threading import Thread
-import time
-
 import cv2
 import numpy as np
 import ffmpeg
 import srt
-from srt import Subtitle
-from PIL import ImageFont, ImageDraw, Image  
+from PIL import ImageFont, ImageDraw, Image
+
 
 class CountsPerSec:
     """
@@ -40,6 +38,7 @@ class CountsPerSec:
     def countsPerSec(self):
         elapsed_time = (datetime.now() - self._start_time).total_seconds()
         return self._num_occurrences / elapsed_time
+
 
 class OsdFont:
 
@@ -68,7 +67,7 @@ class OsdFont:
     def get_srt_font_size(self):
         if self.is_hd():
             return 32
-        else: 
+        else:
             return 24
 
 
@@ -92,13 +91,13 @@ class OSDFile:
         return frame
 
     def read_frame(self):
-        
+
         rawData = self.osdFile.read(self.READ_SIZE)
         if len(rawData) < self.READ_SIZE:
             return False
 
         return Frame(rawData, self.font)
-    
+
     def get_software_name(self):
         match self.fcType:
             case "BTFL":
@@ -107,9 +106,8 @@ class OSDFile:
                 return "Ardupilot"
             case "INAV":
                 return "INav"
-            case _ :
+            case _:
                 return "Unknown"
-
 
 
 class Frame:
@@ -123,10 +121,10 @@ class Frame:
         self.font = font
 
         self.glyph_hide_start = [
-            3, # gps
-            4, # gps
-            16, # home distance
-            345, # alt symb
+            3,  # gps
+            4,  # gps
+            16,  # home distance
+            345,  # alt symb
 
         ]
         self.glyph_hide_len = [
@@ -146,11 +144,12 @@ class Frame:
             glyph_index = index + page * 256
 
             if hide and glyph_index in self.glyph_hide_start:
-                self.curent_mask_index = self.glyph_hide_start.index(glyph_index)
+                self.curent_mask_index = self.glyph_hide_start.index(
+                    glyph_index)
 
             if hide and self.curent_mask_index > -1:
                 if self.curent_mask_counter < self.glyph_hide_len[self.curent_mask_index]:
-                    
+
                     if self.curent_mask_counter > 0:
                         glyph_index = self.mask_glyph_no
                     self.curent_mask_counter += 1
@@ -197,7 +196,8 @@ class VideoFile:
 
     def get_size(self):
         width = self.videoFile.get(cv2.CAP_PROP_FRAME_WIDTH)  # float `width`
-        height = self.videoFile.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+        height = self.videoFile.get(
+            cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
         return int(height), int(width)
 
     def get_total_frames(self):
@@ -211,7 +211,7 @@ class VideoFile:
         ret, frame = self.videoFile.read()
         if not ret:
             return None
-        
+
         if len(frame) == 0:
             return None
 
@@ -219,7 +219,7 @@ class VideoFile:
 
 
 class OsdGenConfig:
-    def __init__(self, video_path, osd_path, font_path, srt_path, output_path, offset_left, offset_top, osd_zoom, render_upscale, include_srt, hide_sensitive_osd) -> None: 
+    def __init__(self, video_path, osd_path, font_path, srt_path, output_path, offset_left, offset_top, osd_zoom, render_upscale, include_srt, hide_sensitive_osd) -> None:
         self.video_path = video_path
         self.osd_path = osd_path
         self.font_path = font_path
@@ -231,6 +231,7 @@ class OsdGenConfig:
         self.render_upscale = render_upscale
         self.include_srt = include_srt
         self.hide_sensitive_osd = hide_sensitive_osd
+
 
 class OsdGenStatus:
     def __init__(self) -> None:
@@ -246,15 +247,17 @@ class OsdGenStatus:
     def is_complete(self) -> bool:
         return self.current_frame >= self.total_frames
 
+
 class Utils:
-  
+
     @staticmethod
     def merge_images(img, overlay, x, y, zoom):
-        scale_percent = zoom # percent of original size
+        scale_percent = zoom  # percent of original size
         width = int(overlay.shape[1] * scale_percent / 100)
         height = int(overlay.shape[0] * scale_percent / 100)
         dim = (width, height)
-        img_overlay_res = cv2.resize(overlay, dim, interpolation = cv2.INTER_CUBIC)
+        img_overlay_res = cv2.resize(
+            overlay, dim, interpolation=cv2.INTER_CUBIC)
         # img_crop = img_overlay_res[y:img.shape[0],x:img.shape[1]]
 
         # Image ranges
@@ -271,15 +274,15 @@ class Utils:
         img_crop[:] = img_overlay_crop + img_crop
         img = img_crop
         # img[y:y+img_crop.shape[0], x:x+img_crop.shape[1]] = img_crop
-        
 
     @staticmethod
     def overlay_image_alpha(img, img_overlay, x, y, zoom):
-        scale_percent = zoom # percent of original size
+        scale_percent = zoom  # percent of original size
         width = int(img_overlay.shape[1] * scale_percent / 100)
         height = int(img_overlay.shape[0] * scale_percent / 100)
         dim = (width, height)
-        img_overlay_res = cv2.resize(img_overlay, dim, interpolation = cv2.INTER_CUBIC)
+        img_overlay_res = cv2.resize(
+            img_overlay, dim, interpolation=cv2.INTER_CUBIC)
 
         # Mask
         alpha_mask = img_overlay_res[:, :, 3] / 255.0
@@ -308,22 +311,20 @@ class Utils:
     @staticmethod
     def overlay_srt_line(img, line, font_size, left_offset):
         pos_calc = (left_offset, img.shape[0] - 15)
-        pil_im = Image.fromarray(img)  
-        draw = ImageDraw.Draw(pil_im, 'RGBA')  
-        font = ImageFont.truetype("font.ttf", font_size)  
-        
+        pil_im = Image.fromarray(img)
+        draw = ImageDraw.Draw(pil_im, 'RGBA')
+        font = ImageFont.truetype("font.ttf", font_size)
+
         # left, top, right, bottom = draw.textbbox(pos_calc, line, font=font, anchor="lb")
         # draw.rectangle((left-5, top-5, right+5, bottom+5), fill=(0, 0, 0, 125))
-        draw.text(pos_calc, line, font=font, fill=(255, 255, 255, 255), anchor="lb")
+        draw.text(pos_calc, line, font=font, fill=(
+            255, 255, 255, 255), anchor="lb")
         return Utils.to_numpy(pil_im)
-
 
         # pos_calc = (20, img.shape[0] - 30)
         # cv2.putText(img, line, pos_calc, cv2.FONT_ITALIC, 1/10 * font_size, (255, 255, 255, 255), 1)
 
         # return img
-
-
 
     @staticmethod
     def to_numpy(im):
@@ -345,12 +346,13 @@ class Utils:
         if s < 0:
             raise RuntimeError("encoder error %d in tobytes" % s)
         return data
+
+
 class OsdPreview:
 
     def __init__(self, config: OsdGenConfig):
         self.stopped = False
-        
-        
+
         self.font = OsdFont(config.font_path)
         self.osd = OSDFile(config.osd_path, self.font)
         self.video = VideoFile(config.video_path)
@@ -360,7 +362,6 @@ class OsdPreview:
             self.srt = None
         self.output = config.output_path
         self.config = config
-
 
     def str_line_to_glyphs(self, line):
         filler = self.font.get_glyph(32)
@@ -376,7 +377,6 @@ class OsdPreview:
 
         return glyphs[:53]
 
-
     def generate_preview(self, osd_pos, osd_zomm):
 
         video_frame = self.video.read_frame().data
@@ -386,42 +386,52 @@ class OsdPreview:
             if self.srt:
                 srt_data = self.srt.next_data()
 
-        osd_frame_glyphs =  self.osd.read_frame().get_osd_frame_glyphs(hide=self.config.hide_sensitive_osd)
+        osd_frame_glyphs = self.osd.read_frame().get_osd_frame_glyphs(
+            hide=self.config.hide_sensitive_osd)
 
-        osd_frame = cv2.vconcat([cv2.hconcat(im_list_h) for im_list_h in osd_frame_glyphs])
+        osd_frame = cv2.vconcat([cv2.hconcat(im_list_h)
+                                for im_list_h in osd_frame_glyphs])
         if self.srt and self.config.include_srt:
             srt_line = srt_data["line"]
-            video_frame = Utils.overlay_srt_line(video_frame, srt_line, self.font.get_srt_font_size(), (150 if self.font.is_hd() else 100))
-        Utils.overlay_image_alpha(video_frame, osd_frame, osd_pos[0], osd_pos[1], osd_zomm)
-        result = cv2.resize(video_frame, (640, 360), interpolation = cv2.INTER_AREA)
+            video_frame = Utils.overlay_srt_line(
+                video_frame, srt_line, self.font.get_srt_font_size(), (150 if self.font.is_hd() else 100))
+        Utils.overlay_image_alpha(
+            video_frame, osd_frame, osd_pos[0], osd_pos[1], osd_zomm)
+        result = cv2.resize(video_frame, (640, 360),
+                            interpolation=cv2.INTER_AREA)
         result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
-
-        
-
 
         return result
         # return result
+
+
 class SrtFile():
     def __init__(self, path):
         self.index = 0
         with open(path, "r") as f:
             self.subs = list(srt.parse(f, True))
-            
-    
+
     def next_data(self) -> dict:
         if self.index >= len(self.subs):
-            self.index = len(self.subs) -1
+            self.index = len(self.subs) - 1
         sub = self.subs[self.index]
         data = dict(x.split(":") for x in sub.content.split(" "))
-        d= dict()
+        d = dict()
         d["startTime"] = sub.start.seconds / 1000 * sub.start.microseconds
-        d["data"] =  data #sub.start.seconds / 1000 * sub.start.microseconds
-        d["line"] = "Signal:%1s   Delay:%5s   Bitrate:%7s   Disatnce:%5s" % (data["Signal"], data["Delay"],  data["Bitrate"], data["Distance"])
+        d["data"] = data  # sub.start.seconds / 1000 * sub.start.microseconds
+        d["line"] = "Signal:%1s   Delay:%5s   Bitrate:%7s   Disatnce:%5s" % (
+            data["Signal"], data["Delay"],  data["Bitrate"], data["Distance"])
         self.index += 1
         return d
 
 
-        
+class ThreadPoolExecutorWithQueueSizeLimit(ThreadPoolExecutor):
+    def __init__(self, maxsize=50, *args, **kwargs):
+        super(ThreadPoolExecutorWithQueueSizeLimit,
+              self).__init__(*args, **kwargs)
+        self._work_queue = queue.Queue(maxsize=maxsize)
+
+
 class OsdGenerator:
 
     def __init__(self, config: OsdGenConfig):
@@ -444,8 +454,6 @@ class OsdGenerator:
         except:
             pass
 
-
-
     def start_video(self, upscale: bool):
         Thread(target=self.render, args=()).start()
         return self
@@ -459,7 +467,8 @@ class OsdGenerator:
 
     @staticmethod
     def __render_osd_frame(osd_frame_glyphs):
-        render = cv2.vconcat([cv2.hconcat(im_list_h) for im_list_h in osd_frame_glyphs])
+        render = cv2.vconcat([cv2.hconcat(im_list_h)
+                             for im_list_h in osd_frame_glyphs])
         return render
 
     def __overlay_osd(self, video_frame, osd_frame):
@@ -482,13 +491,12 @@ class OsdGenerator:
         else:
             ff_size = {"w": video_size[1], "h": video_size[0]}
 
-
         out_path = os.path.join(self.output, "ws_%09d.png")
         osd_frame = (
             ffmpeg
             .input(out_path, framerate=60)
             .filter("scale", **ff_size, force_original_aspect_ratio=0)
-            
+
         )
 
         input_args = {
@@ -505,8 +513,6 @@ class OsdGenerator:
 
         self.render_done = False
 
-
-
         output_args = {
             "vcodec": "hevc_nvenc",
             "c:v": "hevc_nvenc",
@@ -516,39 +522,40 @@ class OsdGenerator:
             "acodec": "copy"
         }
 
-
         process = (
             video
             .filter("pad", **ff_size, x=-1, y=-1, color="black")
             .overlay(osd_frame, x=0, y=0)
             .output("%s_osd.mp4" % (self.output),  **output_args)
-            .overwrite_output() 
+            .overwrite_output()
             .run()
         )
         self.render_done = True
 
-    def main(self): 
+    def main(self):
         cps = CountsPerSec().start()
         pr = cProfile.Profile()
         pr.enable()
 
-        
         osd_time = -1
         osd_frame = []
         current_frame = 1
-        srt_time = -1 
+        srt_time = -1
         video_fps = self.video.get_fps()
         total_frames = self.video.get_total_frames()
         video_size = self.video.get_size()
         img_height, img_width = video_size[0], video_size[1]
         n_channels = 4
-        transparent_img = np.zeros((img_height, img_width, n_channels), dtype=np.uint8) 
+        transparent_img = np.zeros(
+            (img_height, img_width, n_channels), dtype=np.uint8)
         frame = transparent_img.copy()
+        executor = ThreadPoolExecutorWithQueueSizeLimit(
+            max_workers=multiprocessing.cpu_count()-1, maxsize=2000)
+
         while True:
             if self.stopped:
                 print("Process canceled.")
                 break
-
 
             frames_per_ms = 1 / video_fps * 1000
             calc_video_time = int((current_frame - 1) * frames_per_ms)
@@ -566,32 +573,37 @@ class OsdGenerator:
                 if not raw_osd_frame:
                     break
                 frame = transparent_img.copy()
-                osd_frame = self.__render_osd_frame(raw_osd_frame.get_osd_frame_glyphs(hide=self.config.hide_sensitive_osd))
+                osd_frame = self.__render_osd_frame(
+                    raw_osd_frame.get_osd_frame_glyphs(hide=self.config.hide_sensitive_osd))
                 osd_time = raw_osd_frame.startTime
-                Utils.merge_images(frame, osd_frame, self.config.offset_left, self.config.offset_top, self.config.osd_zoom)
+                Utils.merge_images(frame, osd_frame, self.config.offset_left,
+                                   self.config.offset_top, self.config.osd_zoom)
                 if self.srt and self.config.include_srt:
-                    result = Utils.overlay_srt_line(frame, srt_data["line"] , self.font.get_srt_font_size(), (150 if self.font.is_hd() else 100))
+                    result = Utils.overlay_srt_line(frame, srt_data["line"], self.font.get_srt_font_size(
+                    ), (150 if self.font.is_hd() else 100))
                 else:
                     result = frame
 
+            out_path = os.path.join(
+                self.output, "ws_%09d.png" % (current_frame))
+            executor.submit(cv2.imwrite, out_path, result)
 
-            out_path = os.path.join(self.output, "ws_%09d.png" % (current_frame))
-            cv2.imwrite(out_path, result)
-
-
-            current_frame+=1
+            current_frame += 1
             cps.increment()
             fps = int(cps.countsPerSec())
-            self.osdGenStatus.update(current_frame, total_frames, fps)
+            self.osdGenStatus.update(current_frame - 1, total_frames, fps)
 
             if current_frame % 200 == 0:
-                logging.debug("Current: %s/%s (fps: %d)" % (current_frame, total_frames, fps))
-        
+                logging.debug("Current: %s/%s (fps: %d)" %
+                              (current_frame, total_frames, fps))
+
+        logging.info("Waiting for jobs to complete")
+        executor.shutdown(cancel_futures=False, wait=True)
+        logging.info("Save complete")
+        self.osdGenStatus.update(total_frames, total_frames, fps)
         pr.disable()
         s = io.StringIO()
         sortby = SortKey.CUMULATIVE
         ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
         ps.print_stats()
         logging.debug(s.getvalue())
-
-
